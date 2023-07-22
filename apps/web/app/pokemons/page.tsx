38de7591476115f30b028f0f1eaeb0383164f07f
Sahar Levy <sahar.levy@tonkean.com>
2023-07-22 22:30:22 +0300

    add card ui

---
+++ apps/web/app/pokemons/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {Card} from "ui";
+import PokemonSummary from "./_entities/PokemomSummary";
+
+
+async function getData(): Promise<PokemonSummary[]> {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    // Recommendation: handle errors
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return (await res.json()).results
+}
+
+
+interface Props {
+
+}
+
+const Page: React.FC<Props> = async () => {
+    const pokemons = await getData();
+    return (
+        <div style={{display: 'flex', gap: "16px", flexWrap: 'wrap', justifyContent: 'center', marginTop: "16px"}}>
+            {pokemons.map(pokemon =>
+                <Card href={`/pokemons/${pokemon.name}`} key={pokemon.name}>{pokemon.name.toUpperCase()}</Card>
+            )}
+        </div>
+    )
+
+}
+
+
+export default Page;
---
+++ packages/ui/Card.tsx
@@ -0,0 +1,79 @@
+"use client"
+import React from "react";
+import styled from "styled-components";
+import Link from 'next/link'
+
+
+const CardView = styled(Link)`
+  position: relative;
+  width: 220px;
+  height: 320px;
+  background: mediumturquoise;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+  font-weight: bold;
+  border-radius: 15px;
+  cursor: pointer;
+  text-decoration: none;
+  color: black;
+  border: 1px solid black;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+
+
+  &::before,
+  &::after {
+    position: absolute;
+    content: "";
+    width: 20%;
+    height: 20%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    font-weight: bold;
+    background-color: lightblue;
+    transition: all 0.5s;
+  }
+
+  &::before {
+    top: 0;
+    right: 0;
+    border-radius: 0 15px 0 100%;
+  }
+
+  &::after {
+    bottom: 0;
+    left: 0;
+    border-radius: 0 100% 0 15px;
+  }
+
+  &:hover::before,
+  &:hover:after {
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    transition: all 0.5s;
+  }
+
+  &:hover:after {
+    content: 'View'
+
+  }
+`
+
+
+interface Props {
+    // todo use real types
+    href: string;
+    replace?: boolean
+}
+
+const Card: React.FC<React.PropsWithChildren<Props>> = ({children, ...props}) => {
+
+
+    return <CardView {...props}>{children}</CardView>
+}
+
+export default Card
