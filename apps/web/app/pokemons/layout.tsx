e8e13b91d5d5e908a0e012f01a80276f41c8c50b
Sahar Levy <sahar.levy@tonkean.com>
2023-07-22 22:32:07 +0300

    add navbar to pokemon layouts

---
+++ apps/web/app/pokemons/layout.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import styled from "styled-components";
+
+const Navbar = styled.div`
+  height: 64px;
+  padding: 0 24px;
+  border-bottom: 1px solid #ebebeb;
+  width: 100%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: sticky;
+  top: 0;
+  z-index: 3000;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+`
+
+const Layout = ({children}) => {
+
+
+    return <>
+        <Navbar>
+            <div>Pokemons!</div>
+        </Navbar>
+
+        {children}
+    </>
+}
+
+export default Layout;
