b0914395c9c85ef6e4fa821e05adcc3ef50e6dba
Sahar Levy <sahar.levy@tonkean.com>
2023-07-22 22:57:50 +0300

    change font

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -9,7 +9,10 @@ export default function RootLayout
 }) {
     return (
         <html lang="en">
-        <body style={{margin: 0}}>
+        <body style={{
+            margin: 0,
+            fontFamily: `Montserrat, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", Segoe UI Symbol, "Noto Color Emoji" !important`
+        }}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         </body>
         </html>
